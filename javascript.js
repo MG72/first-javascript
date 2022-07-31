1979d40d020f0fdd159f302809d14be740a78be8
Maarten <mgijselman@gmail.com>
2022-07-31 22:19:34 +0200

    done some exercises with numbers

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let message;
+message = "Yep, you found it, this is the console.log message to the developer";
+console.log(message);
+let name = 'John';
+let admin;
+admin = name;
+console.log(admin);
+
+let ourNamePlanet = "earth";
+let currentUserName = "John";
+console.log(ourNamePlanet);
+
+
+
